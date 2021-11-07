e7bf0af75dc70d3022d53cbb016f7f364df7e88e
mukeshbhatt12344@gmail.com <mukeshbhatt12344@gmail.com>
2021-11-07 20:25:59 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+
+  <signin />
+</div>
+</template>
+
+<script>
+import signin from './components/signin.vue'
+
+export default {
+  name: 'App',
+  components: {
+signin
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/signin.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+     <form  @submit.prevent="login">
+         <input type="text" name="author" placeholder="author" v-model="posts.author"><br>
+         <input type="text" name="title" placeholder="title" v-model="posts.title"><br><br>
+         <button type="submit">Post</button>
+    </form>
+</div>
+</template>
+<script>
+
+
+export default {
+name:"signin",
+data(){
+    return{
+        posts:{
+            title:'',
+            author:''
+        }
+    }
+},
+methods:{
+    login()
+    {
+      this.axios.post("http://localhost:3000/posts/",this.posts)
+      .then((result)=>{
+          console.log(result);
+      });
+    }
+    }
+}
+
+
+
+    
+
+
+
+
+</script>
